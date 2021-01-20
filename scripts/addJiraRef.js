58eba31278cffd1c1ad04acacc2e4b511ad0fc77
Louise Reid <l_reid@hotmail.co.uk>
2021-01-20 12:15:07 +0000

    test

--- scripts/addJiraRef.js
+++ scripts/addJiraRef.js
@@ -14,29 +14,29 @@ exec("git rev-parse --abbrev-ref HEAD", (err, branchName, stderr) => {
   exitOnError(err);
   exitOnError(stderr);
 
-  const branchTicketMatch = branchName.match(/^[A-Z][A-Z0-9_]+-\d+/);
+  // const branchTicketMatch = branchName.match(/^[A-Z][A-Z0-9_]+-\d+/);
 
   const excludedBranches = ["master", "dev", "development"];
   if (excludedBranches.includes(branchName.trim())) {
     process.exit(0);
   }
 
-  if (!branchTicketMatch) {
-    exitOnError("Jira ticket ID not found in branch name");
-  } else {
-    const jiraId = branchTicketMatch[0];
+  // if (!branchTicketMatch) {
+  //   exitOnError("Jira ticket ID not found in branch name");
+  // } else {
+  // const jiraId = branchTicketMatch[0];
 
-    const messageFile = process.env.HUSKY_GIT_PARAMS;
-    const message = fs.readFileSync(messageFile, { encoding: "utf-8" });
-    const messageTitle = message.split("\n")[0];
+  const messageFile = process.env.HUSKY_GIT_PARAMS;
+  const message = fs.readFileSync(messageFile, { encoding: "utf-8" });
+  const messageTitle = message.split("\n")[0];
 
-    if (!message.startsWith(jiraId)) {
-      const messageLines = message.split("\n");
-      messageLines[0] = `${jiraId}: ${messageTitle}`;
+  if (!message.startsWith(branchName)) {
+    const messageLines = message.split("\n");
+    messageLines[0] = `${branchName}: ${messageTitle}`;
 
-      fs.writeFileSync(messageFile, messageLines.join("\n"), {
-        encoding: "utf-8",
-      });
-    }
+    fs.writeFileSync(messageFile, messageLines.join("\n"), {
+      encoding: "utf-8",
+    });
   }
+  // }
 });
